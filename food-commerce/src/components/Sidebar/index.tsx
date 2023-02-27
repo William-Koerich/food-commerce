4b27a34d3affa47d23a0f21a2a20dfc45395d1e3
William Koerich <williamkoerich17@gmail.com>
2023-02-27 11:31:55 -0300

    feat: created sidebar component

---
+++ food-commerce/src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+
+export function Sidebar () {
+    return (
+        <Container>
+            Sidebar
+        </Container>
+    )
+    
+}
---
+++ food-commerce/src/components/Sidebar/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.aside`
+    background-color: ${({theme}) => theme.colors.red};
+
+    width: 7.75rem;
+
+    padding: 2rem 0;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
