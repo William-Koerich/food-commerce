3f3bc63eb663d844466da373e1690e55703802f3
William Koerich <williamkoerich17@gmail.com>
2023-02-27 13:16:30 -0300

    feat: created routes

--- food-commerce/src/routes.tsx
+++ food-commerce/src/routes.tsx
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
+import  BurgersPage  from './pages/Burgers'
+import PizzasPage from './pages/Pizzas'
+import DrinksPage from './pages/Drinks'
+import IceCreamsPage from './pages/IceCreams'
 
 import MainPage from './pages/Main'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />}>
+        <Route path='/' element={<BurgersPage />} />
+        <Route path='pizzas' element={<PizzasPage />} />
+        <Route path='drinks' element={<DrinksPage />} />
+        <Route path='ice-creams' element={<IceCreamsPage />} />
+      </Route>
     </Routes>
   )
 }
