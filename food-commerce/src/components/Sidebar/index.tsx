55a9f57ad6c3ca95d2cac93c378927733edf89f2
William Koerich <williamkoerich17@gmail.com>
2023-02-27 12:50:26 -0300

    feat: created sidebar component

--- food-commerce/src/components/Sidebar/index.tsx
+++ food-commerce/src/components/Sidebar/index.tsx
@@ -1,9 +1,57 @@
+import { useState } from "react"
 import { Container } from "./styles"
 
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
+
+
+import menuImg from '../../assets/menu.svg'
+
 export function Sidebar () {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
-        <Container>
-            Sidebar
+        <Container isMenuOpen={menuOpen}>
+            <button onClick={handleToggleMenu}>
+                <img src={menuImg} alt="Abrir e fechar o menu" />
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <BurgerIcon />
+                            <span>El Toro</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <PizzaIcon />
+                            <span>Dona Floripa</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <SodaPopIcon />
+                            <span>Botequim da Brava</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <IceCreamIcon />
+                            <span>Cravo e Canela</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </Container>
     )
     
