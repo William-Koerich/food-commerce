708310b2360036578dc8f9b738ee672a9dbc6162
William Koerich <williamkoerich17@gmail.com>
2023-04-04 11:02:25 -0300

    feat: created confirm order component

---
+++ food-commerce/src/components/ConfirmOrder/index.tsx
@@ -0,0 +1,20 @@
+import { currencyFormat } from "../../helpers/currencyFormat";
+import { useCart } from "../../hooks/useCart";
+import { Container } from "./styles";
+
+export function ConfirmOrder() {
+    const { cart } = useCart()
+
+    const totalAmount = cart.reduce((acc, item) => (acc += item.subtotal), 0)
+
+    return (
+        <Container>
+            <button type="button">
+                Finalizar Pedido
+            </button>
+            <span>
+                Total <strong>{currencyFormat(totalAmount)}</strong>
+            </span>
+        </Container>
+    )
+}
