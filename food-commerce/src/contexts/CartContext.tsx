32edc4fc0407f545ce30f251c5543fbf06fc222c
William Koerich <williamkoerich17@gmail.com>
2023-04-04 11:00:12 -0300

    feat: created cart context

--- food-commerce/src/contexts/CartContext.tsx
+++ food-commerce/src/contexts/CartContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
+import { createContext, ReactNode, useState } from 'react'
+import { toast } from 'react-toastify'
+import { snackEmoji } from '../helpers/snackEmoji';
+
 import { SnackData } from '../interfaces/SnackData';
 
 interface Snack extends SnackData {
@@ -6,22 +9,13 @@ interface Snack extends SnackData {
     subtotal: number
 }
 
-interface RemoveSnackFromCart {
-    id: number
-    snack: string
-}
-
-interface UpdateCartProps {
-    id: number
-    snack: string
-    newQuantity: number
-}
-
 interface CartContextProps {
     cart: Snack[]
     addSnackIntoCart: (snack: SnackData) => void
-    removeSnackFromCart: ({id, snack}: RemoveSnackFromCart) => void
-    updateCart: ({id, snack, newQuantity}: UpdateCartProps) => void
+    removeSnackFromCart: (snack: Snack) => void
+    snackCartIncrement: (snack: Snack) => void
+    snackCartDecrement: (snack: Snack) => void
+    confirmOrder: () => void
 }
 
 interface CartProviderProps {
@@ -31,5 +25,80 @@ interface CartProviderProps {
 export const CartContext = createContext({} as CartContextProps)
 
 export function CartProvider({children}: CartProviderProps){
-    //
+    const [cart, setCart] = useState<Snack[]>([])
+
+    function addSnackIntoCart(snack:SnackData): void {
+        const snackExistentInCart = cart.find((item) => item.snack === snack.snack && item.id === snack.id)
+
+        if (snackExistentInCart) {
+            const newCart  = cart.map((item) => {
+                if (item.id === snack.id) {
+                    const quantity = item.quantity + 1
+                    const subtotal = item.price * quantity
+
+                    return {...item, quantity, subtotal}
+                }
+
+                return item
+            })
+            
+            toast.success(`Outro(a) ${snackEmoji(snack.snack)} ${snack.snack} ${snack.name} adicionado nos pedidos!`)
+            setCart(newCart)
+
+            return
+        }
+
+        const newSnack = { ...snack, quantity: 1, subtotal: snack.price }
+        const newCart = [...cart, newSnack]
+
+        toast.success(`${snackEmoji(snack.snack)} ${snack.snack} ${snack.name} adicionado nos pedidos!`)
+        setCart(newCart)
+    }
+
+    function removeSnackFromCart(snack: Snack) {
+        const newCart = cart.filter((item) => !(item.id === snack.id && item.snack === snack.snack))
+
+        setCart(newCart)
+    }
+
+    function updateSnackQuantity(snack: Snack, newQuantity: number) {
+        if (newQuantity <= 0) return
+
+        const snackExistentInCart = cart.find((item) => item.id === snack.id && item.snack === snack.snack)
+        
+        if (!snackExistentInCart) return
+        
+        const newCart = cart.map((item) => {
+            if (item.id === snackExistentInCart.id && snack.snack === snackExistentInCart.snack) {
+                return {
+                    ...item,
+                    quantity: newQuantity,
+                    subtotal: item.price * newQuantity
+                }
+            }
+
+            return item
+        })
+
+        setCart(newCart)
+    }
+
+    function snackCartIncrement(snack: Snack) {
+        updateSnackQuantity(snack, snack.quantity + 1)
+    }
+
+    function snackCartDecrement(snack: Snack) {
+        updateSnackQuantity(snack, snack.quantity - 1)
+    }
+
+    function confirmOrder () {
+        //
+    }
+
+    return (
+        <CartContext.Provider value={{cart, addSnackIntoCart, removeSnackFromCart, snackCartIncrement, snackCartDecrement, confirmOrder}}>
+            {children}
+        </CartContext.Provider>
+    )
+
 }
